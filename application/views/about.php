72ed6e94b1176c544beee7032feb8a48dd4e4617
musa.nz <126410153+safaRidwan@users.noreply.github.com>
2023-11-23 13:32:18 +0700

    new sandpaper update kamis

--- application/views/about.php
+++ application/views/about.php
@@ -32,11 +32,6 @@
 </head>
 
 <body>
-    <!-- Spinner Start -->
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-grow text-primary" role="status"></div>
-    </div>
-    <!-- Spinner End -->
 
 
     <!-- Topbar Start -->
